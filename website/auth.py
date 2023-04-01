eb89c9d790125a18ac8bb0b94cc3a6d70d0881e9
jun447 <fa20-bse-054@cuilahore.edu.pk>
2023-04-01 23:53:19 +0500

    user created and password is hashed

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,10 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 
+from . import db
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+
 auth = Blueprint('/auth', __name__)
 
 
@@ -31,7 +36,11 @@ def sign_up():
             flash('Password must be at least 7 characters.', category='error')
         else:
             # add user to database
-
+            new_user = User(email=email, first_name=first_name,
+                            password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
